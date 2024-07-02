355d671553954a218b9477767d1f3425a89ac3bc
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-07-02 02:16:40 +0200

    refactor: Refactor Error Messages

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Marcin Jędrzejak on 02/07/2024.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
