1a05dfaa37e0ac71848ef0e46a0c98f93959190e
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-07-06 23:37:16 +0200

    feat: Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Marcin Jędrzejak on 06/07/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
